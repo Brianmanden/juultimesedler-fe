8625c45c4ec9543bdc7ee21a22085dae248ecc23
Brian Andersen <brianmanden@gmail.com>
2022-03-29 18:19:53 +0200

    Debug

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 //Install express server
-// const express = require('express');
-console.log("-1->>>");
+const express = require('express');
 const path = require('path');
-// const app = express();
+const app = express();
 
 console.log("-1->>>" + path);
 
-console.log( __dirname+'/src/index.html' );
+console.log( __dirname+'/../index.html' );
 
 // Serve only the static files form the dist directory
-// app.use( express.static( __dirname + '/src') );
+app.use( express.static( __dirname + '/..') );
 
-// app.get('/*', function(req,res) {
-//     res.sendFile( path.join(__dirname+'/src/index.html') );
-// });
+app.get('/*', function(req,res) {
+    res.sendFile( path.join(__dirname+'/../index.html') );
+});
 
 // Start the app by listening on the default Heroku port
-// app.listen( process.env.PORT || 8080 );
+app.listen( process.env.PORT || 8080 );
